9850958db795609576a2af66ec9fae34bfa46e0e
Florent <fgrenier37@gmail.com>
2021-02-04 19:49:48 +0100

    Ajoutez des fichiers projet.

---
+++ FGR_Perceptron/MyPerceptron.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FGR_Perceptron
+{
+
+    public class MyPerceptron
+    {
+        public int[,] Inputs { get; set; }
+        // Sortie observés
+        public int[] ObservedOutputs { get; set; }
+        // Sortie attendues
+        public int[] ExcepetedOutputs { get; set; }
+        public Double[] Weights { get;  set; }
+        public double LearningRate { get; set; }
+        public double TotalError { get; set; }
+        public double Bias { get; set; }
+
+        public int LenghtArrayInput { get; set; }
+
+
+        public void Perceptron(int[,] inputs, int[,] excepetedoutputs)
+        {
+            
+            Inputs = inputs;
+
+            // HyperParameters
+            LearningRate = 1;
+            TotalError = 1;
+            Bias = 1;
+
+            LenghtArrayInput = LengthDimensionArray(Inputs, 1);
+
+            SynaspseWeights(LenghtArrayInput);
+       
+        }
+
+        /// <summary>
+        /// Obtient le nombre d'élèments dans une dimension d'un tableau multidimensionnel 
+        /// </summary>
+        /// <param name="array">Tableau attendus</param>
+        /// <returns>Retourne le nombre d'élèments dans une dimension</returns>
+        public  int LengthDimensionArray(Array array, int dimension)
+        {
+            int lenght = 0;
+
+            lenght = array.GetLength(dimension);
+
+            return lenght;
+
+        }
+
+        public void SynaspseWeights(int numbSynaspse)
+        {
+            Random r = new Random();
+
+            for (int i = 0; i < numbSynaspse; i++)
+            {
+                Weights[i] = r.NextDouble();
+            }
+
+        }
+
+        public Double CalculOutput()
+        {
+            Double output = 0;
+
+            for (int i = 0; i < Inputs.Length; i++)
+            {
+                for (int j = 0; j < Inputs.Length; j++)
+                {
+                    for (int k = 0; k < Weights.Length; k++)
+                    {
+                        output += (Inputs[i, j] * Weights[k]);
+                    }
+                }
+            }
+
+            return output
+        }
+
+        public void LearningRule()
+        {
+            List<double> correctedWeight = new List<double>(); 
+
+            for (int i = 0; i < Weights.Count(); i++)
+            {
+                correctedWeight.Add(Weights[i] + LearningRate * (ExcepetedOutputs[i] - ObservedOutputs[i]) *  // Entrée du poids i pour la sortie attentue);
+            }
+        }
+    }
+}
---
+++ FGR_Perceptron/TestPerceptron.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FGR_Perceptron
+{
+    class TestPerceptron
+    {
+
+        private MyPerceptron monPerceptron;
+
+        static void Main(string[] args)
+        {
+            #region OtherPerceptron
+            //    int[,] input = new int[,] { { 1, 0 }, { 1, 1 }, { 0, 1 }, { 0, 0 } };
+            //    int[] outputs = { 0, 1, 0, 0 };
+
+            //    Random r = new Random();
+
+            //    double[] weights = { r.NextDouble(), r.NextDouble(), r.NextDouble() };
+
+            //    double learningRate = 1;
+            //    double totalError = 1;
+            //    double bias = 1;
+
+            //    while (totalError > 0.2)
+            //    {
+            //        totalError = 0;
+
+            //        for (int i = 0; i < 4; i++)
+            //        {
+            //            int output = CalculateOutput(input[i, 0], input[i, 1], weights);
+
+            //            int error = outputs[i] - output;
+
+            //            weights[0] += learningRate * error * input[i, 0];
+            //            weights[1] += learningRate * error * input[i, 1];
+            //            weights[2] += learningRate * error * bias;
+
+            //            totalError += Math.Abs(error);
+
+            //        }
+            //    }
+
+            //    Console.WriteLine("Results :");
+            //    for (int i = 0; i < 4; i++)
+            //    {
+            //        Console.WriteLine(totalError);
+
+            //        Console.ReadLine();
+            //    }
+            //}
+
+            //private static int CalculateOutput(double input1, double input2, double[] weights)
+            //{
+            //    double bias = 1;
+
+            //    double sum = input1 * weights[0] + input2 * weights[1] + bias * weights[2];
+
+            //    return (sum >= 0) ? 1 : 0;
+            //}
+            #endregion
+
+            int[,] Inputs = new int[,] { { 0, 0, 1, 0 }, { 1, 1, 1, 0 }, { 1, 0, 1, 1 }, { 0, 1, 1, 1 }, { 0, 1, 0, 1 }, { 1, 1, 1, 1 }, { 0, 0, 0, 0 } };
+            int[,] ExcepetedOutputs = new int[,] { { 0, 1, 1, 0, 0, 1, 0 } };
+        }
+    }
+}
